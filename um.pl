6b2d1a80becf6e153a714f9ba65a8369a4f975b4
Marcelo Honda Kobayashi <139459421+MarceloKobayashi@users.noreply.github.com>
2024-10-11 08:16:14 -0300

    Update um.pl

--- um.pl
+++ um.pl
@@ -14,9 +14,9 @@ show_parents(Parents, X) :-
     setof(P, (father(P, X); mother(P, X)), Parents).
 
 grandfather(X, Y) :- 
-    father(X, Z), (father(Z, Y); mother(Z, Y)). % Se X for pai de Z e Z for pai/mae de Y, então X é avô de Y
+    father(X, Z), (father(Z, Y); mother(Z, Y)).
 all_grandfathers(Grandfathers, X) :-
-    setof(G, grandfather(G, X), Grandfathers). % Acha todos os G avôs de X e coloca em Grandfathers
+    setof(G, grandfather(G, X), Grandfathers).
 
 grandmother(X, Y) :-
     mother(X, Z), (father(Z, Y); mother(Z, Y)).
@@ -24,7 +24,7 @@ all_grandmothers(Gm, X) :-
     setof(G, grandmother(G, X), Gm).
 
 siblings(X, Y) :-
-    father(P, X), father(P, Y), X \= Y. % Se o pai de X é o mesmo pai de Y, então X e Y são irmaos
+    father(P, X), father(P, Y), X \= Y.
 siblings(X, Y) :-
     mother(M, X), mother(M, Y), X \= Y.
 all_siblings(Siblings, X) :-
